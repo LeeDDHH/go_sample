6fb7f97fa66c52d65c00f46b2263fae62954d895
DongHeon Lee <niche3600@gmail.com>
2022-04-24 20:01:35 +0900

    switchのfallthroughの例

--- hello.go
+++ hello.go
@@ -59,19 +59,57 @@ func main() {
 	// 		fmt.Println("月の値ではありません")
 	// }
 
-	x := input.Input("type a number")
+	// x := input.Input("type a number")
+	// n, err := strconv.Atoi(x)
+
+	// if err == nil {
+	// 	fmt.Println(x + "は、")
+	// } else {
+	// 	return
+	// }
+	// switch {
+	// 	case n%2 == 0:
+	// 		fmt.Println("偶数")
+
+	// 	case n%2 == 1:
+	// 		fmt.Println("奇数")
+	// }
+
+	x := input.Input("type 1~5")
 	n, err := strconv.Atoi(x)
 
 	if err == nil {
-		fmt.Println(x + "は、")
+		fmt.Println(x + "までの合計は、")
 	} else {
 		return
 	}
-	switch {
-		case n%2 == 0:
-			fmt.Println("偶数")
 
-		case n%2 == 1:
-			fmt.Println("奇数")
+	t := 0;
+
+	switch n {
+		case 5:
+			t += 5
+			fallthrough
+
+		case 4:
+			t += 4
+			fallthrough
+
+		case 3:
+			t += 3
+			fallthrough
+
+		case 2:
+			t += 2
+			fallthrough
+		
+		case 1:
+			t++
+		
+		default:
+			fmt.Println("範囲外です。")
+			return
 	}
+
+	fmt.Println(t, "です")
 }
