ad94934ef2df74e0fc407f13e298c500299ceb8d
DongHeon Lee <niche3600@gmail.com>
2022-04-24 17:57:22 +0900

    型変換の例

--- hello.go
+++ hello.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	input "go_sample/pkg"
+	"strconv"
 )
 
 func main() {
-	name := input.Input("type your name")
-	fmt.Println("Hello, " + name + "!!")
-	a,b,c := 100, 200, 300
-	fmt.Print("total: ")
-	fmt.Println(a+b+c)
+	// name := input.Input("type your name")
+	// fmt.Println("Hello, " + name + "!!")
+	// a,b,c := 100, 200, 300
+	// fmt.Print("total: ")
+	// fmt.Println(a+b+c)
+	x := input.Input("type a price")
+	n, err := strconv.Atoi(x)
+	if err != nil {
+		fmt.Print("Error: ")
+		fmt.Println(err.Error())
+		return
+	}
+	p := float64(n)
+	fmt.Println(int(p*1.1))
 }
