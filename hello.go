8d978b81646be23e044b3e080027064e24d47ec6
DongHeon Lee <niche3600@gmail.com>
2022-04-24 20:14:38 +0900

    forぼ例

--- hello.go
+++ hello.go
@@ -75,40 +75,59 @@ func main() {
 	// 		fmt.Println("奇数")
 	// }
 
-	x := input.Input("type 1~5")
+	// x := input.Input("type 1~5")
+	// n, err := strconv.Atoi(x)
+
+	// if err == nil {
+	// 	fmt.Println(x + "までの合計は、")
+	// } else {
+	// 	return
+	// }
+
+	// t := 0;
+
+	// switch n {
+	// 	case 5:
+	// 		t += 5
+	// 		fallthrough
+
+	// 	case 4:
+	// 		t += 4
+	// 		fallthrough
+
+	// 	case 3:
+	// 		t += 3
+	// 		fallthrough
+
+	// 	case 2:
+	// 		t += 2
+	// 		fallthrough
+		
+	// 	case 1:
+	// 		t++
+		
+	// 	default:
+	// 		fmt.Println("範囲外です。")
+	// 		return
+	// }
+
+	// fmt.Println(t, "です")
+
+	x := input.Input("type a number")
 	n, err := strconv.Atoi(x)
 
 	if err == nil {
-		fmt.Println(x + "までの合計は、")
+		fmt.Println("1から" + x + "の合計は、")
 	} else {
 		return
 	}
 
 	t := 0;
+	c := 1;
 
-	switch n {
-		case 5:
-			t += 5
-			fallthrough
-
-		case 4:
-			t += 4
-			fallthrough
-
-		case 3:
-			t += 3
-			fallthrough
-
-		case 2:
-			t += 2
-			fallthrough
-		
-		case 1:
-			t++
-		
-		default:
-			fmt.Println("範囲外です。")
-			return
+	for c <= n {
+		t += c
+		c++
 	}
 
 	fmt.Println(t, "です")
