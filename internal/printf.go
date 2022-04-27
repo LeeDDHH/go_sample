51b3ebb81bff28781ef4601aa992fa9e1855451b
DongHeon Lee <niche3600@gmail.com>
2022-04-28 08:29:56 +0900

    printf例

---
+++ internal/printf.go
@@ -0,0 +1,10 @@
+package internal
+
+import "fmt"
+
+func PrintfSample() {
+	n := 123
+	b := true
+	s := "hello"
+	fmt.Printf("number:%d, bool:%t, string:%s.",n,b,s)
+}
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	function.Closure()
+	function.PrintfSample()
 }
