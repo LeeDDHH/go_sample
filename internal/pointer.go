ed311c522777f7cd150447cbf14e235d40b48071
DongHeon Lee <niche3600@gmail.com>
2022-04-28 10:47:15 +0900

    ポインタの入れ替え例

--- internal/pointer.go
+++ internal/pointer.go
@@ -9,3 +9,18 @@ func PointerSample() {
 	fmt.Println("pointer: ", p)
 	fmt.Println("value: ", *p)
 }
+
+func PinterSubstitution() {
+	n := 123
+	p := &n
+	m := 10000
+	p2 := &m
+	fmt.Printf("p value:%d, address:%p\n", *p, p)
+	fmt.Printf("p2 value:%d, address:%p\n", *p2, p2)
+
+	pb := p
+	p = p2
+	p2 = pb
+	fmt.Printf("p value:%d, address:%p\n", *p, p)
+	fmt.Printf("p2 value:%d, address:%p\n", *p2, p2)
+}
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	function.PointerSample()
+	function.PinterSubstitution()
 }
